40e1a628d184079d15fe1fff38c6d0811e3485c1
Aycan ÖĞÜT <aycanonfire@gmail.com>
2021-05-10 15:36:29 +0300

    navbar burger-toggler style and functionality updated

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,13 +1,4 @@
-const navSlide = () => {
-  const burger = document.querySelector(".burger-toggler");
-  const nav = document.querySelector(".navbar__collapse");
-  const navLinks = document.querySelectorAll('.navbar__items')
-
-  burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
-  });
-
-
-}
-
-navSlide();
+$('.burger-toggler').click(function () {
+  $('.navbar__items').toggleClass("navbar-toggle-show");
+  return false;
+});
